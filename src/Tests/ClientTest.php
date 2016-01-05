ae1d2949b03c82520ddd78444048032af222d684
Diego Saint Esteben <diego@saintesteben.me>
2016-01-05 18:44:06 -0300

    Added license headings

--- src/Tests/ClientTest.php
+++ src/Tests/ClientTest.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the RingCaptcha library.
+ *
+ * (c) RingCaptcha
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace RingCaptcha\Tests;
 
 class ClientTest extends \PHPUnit_Framework_TestCase
